028d5577dc381b13f5db3024e62058f3298f8f80
Mane-Patil <jagdishmane0457@gmail.com>
2023-10-30 19:02:06 +0530

    massege

--- src/main/java/git_learning/New_Class.java
+++ src/main/java/git_learning/New_Class.java
@@ -8,5 +8,7 @@ public class New_Class {
 		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije");
 		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123");
 		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123");
+		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123");
+		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123333444");
 	}
 }
