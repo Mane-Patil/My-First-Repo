70b9c8a9778c29e547aa961f98611a58ea960f33
Mane-Patil <jagdishmane0457@gmail.com>
2023-10-31 10:04:02 +0530

    Mane PAtil

--- src/main/java/git_learning/New_Class.java
+++ src/main/java/git_learning/New_Class.java
@@ -5,13 +5,8 @@ public class New_Class {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123333444");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije 123333444");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije JARANGE PATIL");
-		System.out.println("Mane PAtil Only ... Maratha Arakshan Milale Pahije JARANGE PATIL 123");
+	
+		System.out.println("test case");
+		
 	}
 }
