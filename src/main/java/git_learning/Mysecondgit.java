6e3f1c64c18ac7e95a9f394016768f6777f620e2
Mane-Patil <jagdishmane0457@gmail.com>
2023-10-31 11:57:44 +0530

    f

--- src/main/java/git_learning/Mysecondgit.java
+++ src/main/java/git_learning/Mysecondgit.java
@@ -5,6 +5,7 @@ public class Mysecondgit {
 	public static void main(String arg []) {
 		System.out.println("second commit");
 		System.out.println("second commit");
+		System.out.println("second commit");
 	}
 	
 }
