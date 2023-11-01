4f356260e3c485e7ab75fbdbc1ec38ed59a301a0
Mane-Patil <149386772+Mane-Patil@users.noreply.github.com>
2023-11-01 09:45:21 +0530

    Update New_Class.java

--- src/main/java/git_learning/New_Class.java
+++ src/main/java/git_learning/New_Class.java
@@ -4,20 +4,7 @@ public class New_Class {
 
 	public static void main(String[] args) {
 		
-		
-	
-		System.out.println("test case");
-		System.out.println("test case");
-		System.out.println("test case");
-		System.out.println("pulle");
-		System.out.println("New Line Added");
-		System.out.println("New Line edit");
-		System.out.println("New Test");
-
-		System.out.println("Added by vishal");
-		System.out.println("Added by vishal 2");
-		
-		System.out.println("Added by jagdish");
+	System.out.println("Added by jagdish");
 		System.out.println("Addby githubh");
 	
 		
